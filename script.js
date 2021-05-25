d6c34a69cd36189dc025f2647ab2b0bf910c8c01
Chenyun Zhang <morningchenyun@gmail.com>
2021-05-25 19:53:23 -0400

    update js

--- script.js
+++ script.js
@@ -1,77 +1,90 @@
-$("button").on("click", handleClick)
-let firstValue = 0,
-  operatorValue = "",
-  firstValueFlag = false,
-  secondValue = 0,
-  result = 0
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
 
-function handleClick(e) {
-  // display number
-  if (e.target.classList[0] === "number") {
-    if (firstValueFlag) {
-      $(".display").text(e.target.value)
-      firstValueFlag = false
-    } else {
-      if ($(".display").text() !== "0") {
-        $(".display").text($(".display").text() + e.target.value)
-      } else {
-        $(".display").text(e.target.value)
-      }
-    }
-  }
-  // decimal
-  if (e.target.classList[0] === "decimal") {
-    if (!$(".display").text().includes(".")) {
-      $(".display").text($(".display").text() + e.target.value)
-    }
-  }
-  // clear display
-  if (e.target.classList[0] === "clear") {
-    $(".display").text("0")
-  }
-  // backspace
-  if (e.target.classList[0] === "del") {
-    let arr = $(".display").text().split("")
-    if (arr.length === 1) {
-      $(".display").text("0")
-    } else {
-      arr.pop()
-      $(".display").text(arr.join(""))
-    }
-  }
-  // operators
-  if (e.target.classList[0] === "operator") {
-    operatorValue = e.target.value
-    if (!firstValueFlag) {
-      firstValue = $(".display").text()
-      firstValueFlag = true
-    }
-  }
-  if (e.target.classList[0] === "equal-sign") {
-    getSolution()
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
+function sendNumberValue(number) {
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, if not add number to display value
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
   }
 }
 
-function getSolution(){
-    secondValue = $(".display").text()
-    switch (operatorValue) {
-      case "+":
-        result = (Number(firstValue)+Number(secondValue)).toString()
-        $(".display").text(result)
-        console.log(result)
-        break
-      case "-":
-        result = (Number(firstValue)-Number(secondValue)).toString()
-        $(".display").text(result)
-        break
-      case "*":
-        result = (Number(firstValue)*Number(secondValue)).toString()
-        $(".display").text(result)
-        break
-      case "/":
-        result = (Number(firstValue)/Number(secondValue)).toString()
-        $(".display").text(result)
-        break
+function addDecimal() {
+  // If operator pressed, don't add decimal
+  if (awaitingNextValue) return;
+  // If no decimal, add one
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
+  // Assign firstValue if no value
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
+  // Ready for next value, store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+}
+
+// Add Event Listeners for numbers, operators, decimal
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains('decimal')) {
+    inputBtn.addEventListener('click', () => addDecimal());
+  } else if (inputBtn.classList.contains('del')){
+    inputBtn.addEventListener('click', (e) => backSpace(e));
+  }
+});
+
+function backSpace(e){
+  let arr = calculatorDisplay.textContent.split("")
+  if (arr.length === 1) {
+    calculatorDisplay.textContent=0
+  } else {
+    arr.pop()
+    calculatorDisplay.textContent=arr.join("")
+  }
+}
+// Reset all values, display
+function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = '0';
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
